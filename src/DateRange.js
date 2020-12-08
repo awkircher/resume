0644b9ff3fe0238259d7f1ce902d10650031b779
Ashley Kircher <ashleywkircher@gmail.com>
2020-12-08 12:10:37 -0600

    Year validation for DateRange component. Component now checks that start year comes before end year, if both values have been set.

--- src/DateRange.js
+++ src/DateRange.js
@@ -8,6 +8,7 @@ class DateRange extends React.Component {
             yearStartValue: "",
             monthEndValue: "",
             yearEndValue: "",
+            valid: true,
         };
         this.handleMonthStartChange = this.handleMonthStartChange.bind(this);
         this.handleYearStartChange = this.handleYearStartChange.bind(this);
@@ -19,12 +20,24 @@ class DateRange extends React.Component {
     }
     handleYearStartChange(e) {
         this.setState({yearStartValue: (e.target.value)});
+        this.validate(e.target.value, this.state.yearEndValue);
     }
     handleMonthEndChange(e) {
         this.setState({monthEndValue: (e.target.value)});
     }
     handleYearEndChange(e) {
         this.setState({yearEndValue: (e.target.value)});
+        this.validate(this.state.yearStartValue, e.target.value);
+    }
+    validate(start, end) {
+        if (start === "" || end === "") {
+            return;
+        }
+        else if (end >= start) {
+            this.setState({valid: true});
+        } else {
+            this.setState({valid: false});
+        }
     }
 
     render() {
@@ -33,6 +46,7 @@ class DateRange extends React.Component {
         const yearStart = this.state.yearStartValue;
         const monthEnd = this.state.monthEndValue;
         const yearEnd = this.state.yearEndValue;
+        const valid = this.state.valid;
         if (editable) {
             return (
                <div>
@@ -53,6 +67,7 @@ class DateRange extends React.Component {
                                 <option value="December" />
                             </datalist>
                         <input type="number" min="1935" max="2020" placeholder="Year" onChange={this.handleYearStartChange} value={yearStart}></input>
+                        <div className="error">{valid ? "" : "Enter a valid date range"}</div>
                     </fieldset>
                     <fieldset>
                         <input list="months" value={monthEnd} onChange={this.handleMonthEndChange} />
@@ -71,18 +86,18 @@ class DateRange extends React.Component {
                                 <option value="December" />
                             </datalist>
                         <input type="number" min="1935" max="2020" placeholder="Year" onChange={this.handleYearEndChange} value={yearEnd}></input>
+                        <div className="error">{valid ? "" : "Enter a valid date range"}</div>
                     </fieldset>
                </div> 
             );
         } else {
             return (
                 <div>
-                    <p className="inline">{monthStart ? monthStart : "No start date entered, yet."}</p>
-                    <p className="inline">{yearStart ? yearStart : "No start date entered, yet."}</p>
-                    <p className="inline">{monthEnd ? monthEnd : "No start date entered, yet."}</p>
-                    <p className="inline">{yearEnd ? yearEnd : "No start date entered, yet."}</p>
+                    <p className="inline">{monthStart ? monthStart : "No starting month selected, yet."}</p>
+                    <p className="inline">{yearStart ? yearStart : "No starting year entered, yet."}</p>
+                    <p className="inline">{monthEnd ? monthEnd : "No ending month entered, yet."}</p>
+                    <p className="inline">{yearEnd ? yearEnd : "No ending year entered, yet."}</p>
                 </div>
-         
              );
         }
     }
